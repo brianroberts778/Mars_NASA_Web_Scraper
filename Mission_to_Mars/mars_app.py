3d87a50131e3dce7127726717aeb5a1eaf7649b3
brianroberts778 <70656160+brianroberts778@users.noreply.github.com>
2020-12-14 13:46:20 -0500

    mars_app update

--- Mission_to_Mars/mars_app.py
+++ Mission_to_Mars/mars_app.py
@@ -1,23 +1,27 @@
 from pymongo import MongoClient
 from flask import Flask, render_template, redirect
-import mission_to_mars
+import scrape_mars
 
 conn = MongoClient("mongodb://localhost:27017/mars")
 
 app = Flask(__name__)
 
+# Create route to query mongoDB and pass data into html template
 @app.route("/")
 def index ():
     coll1 = conn.db.coll1.find_one()
     return render_template("index.html", mars_data = coll1) 
 
 @app.route("/scrape")
-def scraper ():
+def scrape():
+
     coll1 = conn.db.coll1
-    scrape_data = mission_to_mars.scrape_info()
+    scrape_data = scrape_mars.scrape()
     coll1.update({},scrape_data, upsert=True)
     return redirect ("/")
 
-app.run(Debug=True)
+if __name__ == "__main__":
+    app.run(Debug=True)
+
 
 
